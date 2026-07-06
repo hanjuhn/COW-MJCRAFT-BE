cea1b0c843691ee8c62ab69575c26490cc6c6abf
yunjin1213 <yunjinkim25@mju.ac.kr>
2026-07-06 22:54:22 +0900

    feat: MinIO 스토리지 설정 추가

--- src/main/java/com/example/cowmjucraft/global/cloud/S3Config.java
+++ src/main/java/com/example/cowmjucraft/global/cloud/S3Config.java
@@ -1,10 +1,12 @@
 package com.example.cowmjucraft.global.cloud;
 
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -13,10 +15,15 @@ public class S3Config {
     @Value("${aws.region}")
     private String region;
 
+    @Value("${aws.s3.endpoint}")
+    private String endpoint;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
+                .endpointOverride(URI.create(endpoint))
+                .serviceConfiguration(s3Configuration())
                 .build();
     }
 
@@ -24,6 +31,14 @@ public class S3Config {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(Region.of(region))
+                .endpointOverride(URI.create(endpoint))
+                .serviceConfiguration(s3Configuration())
+                .build();
+    }
+
+    private S3Configuration s3Configuration() {
+        return S3Configuration.builder()
+                .pathStyleAccessEnabled(true)
                 .build();
     }
 }
